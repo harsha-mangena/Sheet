6d671c1f6c9df28574f817593b859e24ad7b4eb3
harsha-mangena <harsha.mangena@gmail.com>
2022-07-01 17:29:18 +0530

    Initial commit

---
+++ LinkedList/Node.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class Node {
+
+    public int value;
+    public Node next;
+
+    public Node(){}
+
+    public Node(int value){
+        this.value = value;
+        this.next = null;
+    }
+}
---
+++ LinkedList/Remove-Duplicates-In-Sorted-LinkedList.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+class RemoveDuplicatesInASortedList{
+    public static Node removeDuplicates(Node node){
+        //Edge Case
+        if(node==null || node.next==null){
+            return node;
+        }
+
+        Node dummy = new Node(-1);
+        dummy.next = node;
+
+        /*
+         * We have to chck for two nodes at a time, i.e node and node.next .
+         * If node.val == node.next.val, then we will update the current node with next noce next value
+         */
+        while(node!=null && node.next!=null){
+            //If both nodes are equal
+            if(node.value == node.next.value){
+                node.next = node.next.next;
+            }
+            //Else just update the current node with next one.
+            else{
+                node = node.next;
+            }
+        }
+        return dummy.next;
+    } 
+}
+
