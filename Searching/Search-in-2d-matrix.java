d6d899a6c82b13b00a00f78aa50fffa6c3181658
harsha-mangena <harsha.mangena@gmail.com>
2022-07-16 23:10:42 +0530

    Searching Problems

---
+++ Searching/Search-in-2d-matrix.java
@@ -0,0 +1,19 @@
+package Searching;
+
+class Solution{
+    public boolean searchMatrix_BinarySearch(int[][] matrix, int target) {
+        if (matrix.length == 0) return false;
+        int rows = matrix.length, cols = matrix[0].length;
+
+        int lo = 0, hi = rows * cols - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int num = matrix[mid / cols][mid % cols];
+            if (num == target) return true;
+            else if (num < target) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return false;
+    }
+
+}
